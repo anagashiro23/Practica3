3c51b48e6f34db3fe58488979748790e218d5bb7
anagashiro <a2020116626@estudiantes.upsa.edu.bo>
2021-10-13 11:38:01 -0400

    Models Play creado

---
+++ Play/Play/Models/Nintendo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Play.Models
+{
+    public class Song
+    {
+        [Key]
+        public string nombre { get; set; }
+        [Display(Name = "Nombre del juego")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "El nombre debe contener entre 3 a 60 caracteres")]
+        [Required(ErrorMessage = "Empresa del juego")]
+        public string autor { get; set; }
+        [Display(Name = "descripcion del juego")]
+        public string letra { get; set; }
+        [Url]
+        [Display(Name = "Link de una foto o video del juego")]
+        public string link { get; set; }
+    }
+}
